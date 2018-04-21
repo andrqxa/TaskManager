fe5301a241631174c8c9203d30b058c9d8cc816b
Andrey Pugachenko <andrqxa@gmail.com>
2018-04-21 13:58:21 +0300

    Added Task class

---
+++ src/main/java/com/andrqxa/training/taskmanager/manager/Task.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2018 Andrey Pugachenko <andrqxa@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.andrqxa.training.taskmanager.manager;
+
+import java.util.Optional;
+import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * The class which stores task
+ *
+ * @author Andrey Pugachenko <andrqxa@gmail.com>
+ */
+public class Task {
+
+    private final UUID id;
+    private String name;
+    private String description;
+
+    public Task() {
+        this.id = UUID.randomUUID();
+    }
+
+    public String getName() {
+        Optional<String> result = Optional.ofNullable(name);
+        return result.orElse(StringUtils.EMPTY);
+    }
+
+    public void setName(String name) {
+        Optional<String> result = Optional.ofNullable(name);
+        this.name = result.orElse(StringUtils.EMPTY);
+    }
+
+    public String getDescription() {
+        Optional<String> result = Optional.ofNullable(description);
+        return result.orElse(StringUtils.EMPTY);
+    }
+
+    public void setDescription(String description) {
+        Optional<String> result = Optional.ofNullable(name);
+        this.description = result.orElse(StringUtils.EMPTY);
+    }
+
+}
